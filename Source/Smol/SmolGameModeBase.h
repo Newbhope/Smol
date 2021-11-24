4a5c403946b843543db6aff883d9d938a358fd7a
Newbhope <newbhope8@gmail.com>
2021-11-24 13:07:21 -0800

    Smol Initial

---
+++ Source/Smol/SmolGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SmolGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SMOL_API ASmolGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
