9c94673ef45a928cb19fc8b7d71665cc182eb6d5
Newbhope <newbhope8@gmail.com>
2023-03-29 00:48:05 +0900

    Add yuge attack animation and janky AI movement

---
+++ Source/Smol/EnemyAIController.h
@@ -0,0 +1,17 @@
+// Copyright Fried Rice Productions
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "EnemyAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SMOL_API AEnemyAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
