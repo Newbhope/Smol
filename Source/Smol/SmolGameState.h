22bb7fb7be56683cb68a0328251b7f14f9959c48
Newbhope <newbhope8@gmail.com>
2023-07-26 01:45:33 +0900

    Add sprinting and test timer. Move stuff into folders

---
+++ Source/Smol/SmolGameState.h
@@ -0,0 +1,17 @@
+// Copyright Fried Rice Productions
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameState.h"
+#include "SmolGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SMOL_API ASmolGameState : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+};
