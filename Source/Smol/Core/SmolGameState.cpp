b44be61990163ffc40afb9c7e445e8be28cc52b8
Newbhope <newbhope8@gmail.com>
2024-04-15 01:22:04 +0900

    Add countdown timer sequence. Still want to disable controls during it though

--- Source/Smol/Core/SmolGameState.cpp
+++ Source/Smol/Core/SmolGameState.cpp
@@ -3,3 +3,28 @@
 
 #include "SmolGameState.h"
 
+#include "Kismet/KismetSystemLibrary.h"
+
+
+void ASmolGameState::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ASmolGameState::Tick(const float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	if (bGameTimerIsRunning)
+	{
+		const float SecondsElapsed = UKismetSystemLibrary::GetGameTimeInSeconds(this) - GameStartTime;
+		RemainingGameDuration = TotalGameDuration - SecondsElapsed;
+	}
+}
+
+void ASmolGameState::SetAndBeginGameTimer()
+{
+	GameStartTime = UKismetSystemLibrary::GetGameTimeInSeconds(this);
+	bGameTimerIsRunning = true;
+}
--- Source/Smol/Core/SmolGameState.h
+++ Source/Smol/Core/SmolGameState.h
@@ -7,11 +7,39 @@
 #include "SmolGameState.generated.h"
 
 /**
- * 
+ * Keeps track of game timer and other time related things like sunrise
  */
 UCLASS()
 class SMOL_API ASmolGameState : public AGameStateBase
 {
 	GENERATED_BODY()
-	
+
+public:
+	// Base life cycle methods
+	virtual void Tick(float DeltaSeconds) override;
+
+	// Public API methods
+	UFUNCTION(BlueprintCallable, Category="GameTimer")
+	void SetAndBeginGameTimer();
+
+protected:
+	// Base life cycle methods
+	virtual void BeginPlay() override;
+
+
+	// Properties
+
+	// GameTime to count down from for the timer
+	UPROPERTY(BlueprintReadWrite, Category="GameTimer")
+	float GameStartTime = 0;
+
+	UPROPERTY(BlueprintReadWrite, Category="GameTimer")
+	bool bGameTimerIsRunning = false;
+
+	// In seconds, how long until the game over sequence is ran
+	UPROPERTY(BlueprintReadWrite, Category="GameTimer")
+	float TotalGameDuration = 60;
+
+	UPROPERTY(BlueprintReadWrite, Category="GameTimer")
+	float RemainingGameDuration = 0;
 };
