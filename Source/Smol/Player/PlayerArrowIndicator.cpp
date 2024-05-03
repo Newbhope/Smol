4477905f6c5338aaaa73f281e8be3014cca77add
Newbhope <newbhope8@gmail.com>
2024-05-03 23:08:10 +0900

    Add janky arrow indicator

---
+++ Source/Smol/Player/PlayerArrowIndicator.cpp
@@ -0,0 +1,26 @@
+﻿// Copyright Fried Rice Productions
+
+
+#include "PlayerArrowIndicator.h"
+
+
+// Sets default values
+APlayerArrowIndicator::APlayerArrowIndicator()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void APlayerArrowIndicator::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APlayerArrowIndicator::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/Smol/Player/PlayerArrowIndicator.h
@@ -0,0 +1,25 @@
+﻿// Copyright Fried Rice Productions
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PlayerArrowIndicator.generated.h"
+
+UCLASS()
+class SMOL_API APlayerArrowIndicator : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	APlayerArrowIndicator();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
